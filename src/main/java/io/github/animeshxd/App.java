de739a82c6a294d8ab5887bb9703648be9f0f624
Animesh Murmu <am2646374@gmail.com>
2022-08-24 22:58:16 +0530

    init: hibernate sqlite configuration

--- src/main/java/io/github/animeshxd/App.java
+++ src/main/java/io/github/animeshxd/App.java
@@ -1,9 +1,17 @@
 package io.github.animeshxd;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
+        SessionFactory factory = cfg.buildSessionFactory();
+        Session session = factory.openSession();
+
+        session.close();
     }
 }
