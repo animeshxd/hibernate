37b4ae60a593108ed5de565b4147a624c54c629e
Animesh Murmu <am2646374@gmail.com>
2022-08-27 13:13:53 +0530

    Temporal annotation - change Date format DATE, TIME, TIMESTAMP

--- src/main/java/io/github/animeshxd/User.java
+++ src/main/java/io/github/animeshxd/User.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Basic;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import jakarta.persistence.Transient;
 
 @Entity(name = "users_entity")
@@ -23,6 +25,7 @@ public class User {
     private String email;
 
     @Basic(optional = false)
+    @Temporal(TemporalType.DATE)
     private Date date;
 
     public User(int id, String name, int age, String email, Date date) {
