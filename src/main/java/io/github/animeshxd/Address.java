3b11b6344dc365f941407da571ab07e5c1572595
Animesh Murmu <am2646374@gmail.com>
2022-08-29 09:26:12 +0530

    cleanup

--- src/main/java/io/github/animeshxd/Address.java
+++ src/main/java/io/github/animeshxd/Address.java
@@ -1,57 +1,8 @@
 package io.github.animeshxd;
 
 import jakarta.persistence.Embeddable;
-import jakarta.persistence.Transient;
 
 @Embeddable
 public class Address {
 
-    private String village;
-
-    @Transient // this can also have @Transient
-    private String street;
-
-    private int pincode;
-
-    private String state;
-
-    
-    public Address() {}
-
-    public Address(String village, String street, int pincode, String state) {
-        this.village = village;
-        this.street = street;
-        this.pincode = pincode;
-        this.state = state;
-    }
-    public String getVillage() {
-        return village;
-    }
-    public void setVillage(String village) {
-        this.village = village;
-    }
-    public String getStreet() {
-        return street;
-    }
-    public void setStreet(String street) {
-        this.street = street;
-    }
-    public int getPincode() {
-        return pincode;
-    }
-    public void setPincode(int pincode) {
-        this.pincode = pincode;
-    }
-    public String getState() {
-        return state;
-    }
-    public void setState(String state) {
-        this.state = state;
-    }
-    
-    @Override
-    public String toString() {
-        return "Address [pincode=" + pincode + ", state=" + state + ", street=" + street + ", village=" + village + "]";
-    }
-    
 }
--- src/main/java/io/github/animeshxd/App.java
+++ src/main/java/io/github/animeshxd/App.java
@@ -1,7 +1,5 @@
 package io.github.animeshxd;
 
-import java.util.Date;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -16,37 +14,6 @@ public class App
 
         session.beginTransaction();
 
-        var user = new User();
-        user.setIdd(new Idd(1, 1)).setName("abc").setAge(20).setDate(new Date()).setEmail("asdf@ghj.kl");
-        user.setDescription("hello".repeat(25));
-        var addr = new Address();
-        addr.setPincode(123456);
-        addr.setStreet("street");
-        addr.setState("state");
-        addr.setVillage("village");
-        user.setAddress(addr);
-
-        var addr2 = new Address();
-        addr2.setPincode(987654);
-        addr2.setStreet("street 2");
-        addr2.setState("state 2");
-        addr2.setVillage("village 2");
-        user.setAddress2(addr2);
-
-        var addr3 = new Address();
-        addr3.setPincode(123456);
-        addr3.setStreet("street");
-        addr3.setState("state");
-        addr3.setVillage("village");
-        user.setAddress3(addr3);
-
-
-        session.persist(user);
-
-        session.getTransaction().commit();
-
-        user = session.get(User.class, new Idd(1,1));
-        System.out.println(user);
         session.close();
     }
 }
