7c0837c218daadfc7f00a1ac01c5ff973cdf790b
Animesh Murmu <am2646374@gmail.com>
2022-09-08 13:02:28 +0530

    session.remove - will detach object after

--- src/main/java/io/github/animeshxd/App.java
+++ src/main/java/io/github/animeshxd/App.java
@@ -14,21 +14,22 @@ public class App
 
         session.beginTransaction();
 
-        User user = new User("USER A"); //Transient: it is in Transient state untill it got persisted
-        System.out.println("is user persist: "+session.contains(user));
+        User user = new User(); //Transient: it is in Transient state untill it got persisted
+        user.setId(1);
+        System.out.println("is user persist: "+session.contains(user)); // false
         
-        session.persist(user); //Persistent: user will be called persisted object untill it got detached, session.detach(), session.close(), session.clear()
+        session.remove(user); //Persistent: user will be called persisted object untill it got detached, session.detach(), session.close(), session.clear()
                                //with session.persist() any update in user object will be updated when it get commited
                                //session.remove() will detach object and will be removed on commit() and will do nothing if not available
         
-        System.out.println("is user persist: "+session.contains(user)); // true
+        System.out.println("is user persist: "+session.contains(user)); // false
         
         session.getTransaction().commit();
-        System.out.println("is user persist: "+session.contains(user)); // true
+        System.out.println("is user persist: "+session.contains(user)); // false
 
         session.clear(); // Detached: User object will be detached
         System.out.println("is user persist: "+session.contains(user)); // false
-        
+
         session.close();         
         System.out.println("is user persist: "+session.contains(user)); // java.lang.IllegalStateException: Session/EntityManager is closed
     }
