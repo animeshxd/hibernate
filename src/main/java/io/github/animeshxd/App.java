e0974f83a64c8c788555eac5792b7d683beb6e20
Animesh Murmu <am2646374@gmail.com>
2022-09-09 11:30:27 +0530

    _

--- src/main/java/io/github/animeshxd/App.java
+++ src/main/java/io/github/animeshxd/App.java
@@ -14,37 +14,6 @@ public class App
 
         session.beginTransaction();
 
-        User user = new User("A"); //Transient: it is in Transient state untill it got persisted
-        System.out.println("is user persist: "+session.contains(user)); // false
-        
-        session.persist(user); //Persistent: user will be called persisted object untill it got detached, session.detach(), session.close(), session.clear()
-                               //with session.persist() any update in user object will be updated when it get commited
-                               //session.remove() will detach object and will be removed on commit() and will do nothing if not available
-        
-        System.out.println("is user persist: "+session.contains(user)); // true
-        
-        session.getTransaction().commit();
-        System.out.println("is user persist: "+session.contains(user)); // true
-
-        session.clear(); // Detached: all object will be detached
-        System.out.println("is user persist: "+session.contains(user)); // false
-
-        session.close();         
-        //System.out.println("is user persist: "+session.contains(user)); // java.lang.IllegalStateException: Session/EntityManager is closed
-    
-        session = factory.openSession();
-        session.beginTransaction();
-        System.out.println("__new__");
-        System.out.println("is user persist: "+session.contains(user)); // false
-
-        user = session.merge(user); // marge will select first and return Persistent object
-        System.out.println("is user persist: "+session.contains(user)); // true
-        
-        user.setName("B");
-
-        session.getTransaction().commit();
-        System.out.println("is user persist: "+session.contains(user)); // true
-
         session.close();
     }
 }
