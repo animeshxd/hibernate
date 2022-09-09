58b47a7055193acee2ea84a802767a9e5476c563
Animesh Murmu <am2646374@gmail.com>
2022-09-09 22:38:56 +0530

    .

--- src/main/java/io/github/animeshxd/App.java
+++ src/main/java/io/github/animeshxd/App.java
@@ -4,7 +4,7 @@ package io.github.animeshxd;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.query.Query;
+
 
 public class App 
 {
@@ -14,11 +14,7 @@ public class App
         SessionFactory factory = cfg.buildSessionFactory();
         Session session = factory.openSession();
                                          
-        Query<User> query = session.createQuery("from User where id > :id and name = :name", User.class);
-        query.setParameter("id", 5);
-        query.setParameter("name", "X");
-
-        System.out.println(query.list());
+    
         
         session.close();
     }
