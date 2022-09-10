c692427c21ea583c19138dbe9fc35d48c011c7c9
Animesh Murmu <am2646374@gmail.com>
2022-09-10 13:22:10 +0530

    Criteria API - CriteriaBuilder, CriteriaQuery - select from User

--- src/main/java/io/github/animeshxd/App.java
+++ src/main/java/io/github/animeshxd/App.java
@@ -6,6 +6,11 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+
+
 
 public class App 
 {
@@ -14,9 +19,23 @@ public class App
         Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
         SessionFactory factory = cfg.buildSessionFactory();
         Session session = factory.openSession();
-                                        
 
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<User> query = builder.createQuery(User.class);
+        
+        Root<User> root = query.from(User.class);
+        query.select(root); // query.select(query.from(User.class)); // select from User
+        
+        var q = session.createQuery(query);
+        print(q.list());
+        
 
         session.close();
+    }
+
+    static void print(Object ... p){
+        for(Object o: p){
+            System.out.println(o);
+        }
     }
 }
