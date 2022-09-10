fd7c698ac08dcfa68b1d17eaf9fa801e0d23812e
Animesh Murmu <am2646374@gmail.com>
2022-09-10 13:59:46 +0530

    few more CriteriaQuery

--- src/main/java/io/github/animeshxd/App.java
+++ src/main/java/io/github/animeshxd/App.java
@@ -8,6 +8,7 @@ import org.hibernate.cfg.Configuration;
 
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Order;
 import jakarta.persistence.criteria.Root;
 
 
@@ -25,6 +26,20 @@ public class App
         
         Root<User> root = query.from(User.class);
         query.select(root); // query.select(query.from(User.class)); // select from User
+
+        // query.where(
+        //     builder.and(
+        //         builder.gt(root.get("id"), 10), 
+        //         builder.lt(root.get("id"), 16)
+        //     )
+        // )
+        query.where(
+            builder.between(root.get("id"), 10, 16)
+        )
+        .orderBy(
+            builder.desc(root.get("name"))
+        );
+        
         
         var q = session.createQuery(query);
         print(q.list());
