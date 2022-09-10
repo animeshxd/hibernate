2f5e9f8cffa5b55bff9191786f9712368425120a
Animesh Murmu <am2646374@gmail.com>
2022-09-10 13:07:50 +0530

    .

--- src/main/java/io/github/animeshxd/App.java
+++ src/main/java/io/github/animeshxd/App.java
@@ -4,7 +4,7 @@ package io.github.animeshxd;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.query.Query;
+
 
 
 public class App 
@@ -15,17 +15,7 @@ public class App
         SessionFactory factory = cfg.buildSessionFactory();
         Session session = factory.openSession();
                                         
-        Query<User> query = session.createNamedQuery("User.byId", User.class);
-        query.setParameter("id", 12);
-        System.out.println(query.list());
 
-        query = session.createNamedQuery("User.byName", User.class);
-        query.setParameter("name", "C");
-        System.out.println(query.list());
-
-        query = session.createNamedQuery("User.byIdNative", User.class);
-        query.setParameter("id", 5);
-        System.out.println(query.list());
 
         session.close();
     }
--- src/main/java/io/github/animeshxd/User.java
+++ src/main/java/io/github/animeshxd/User.java
@@ -5,15 +5,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.NamedNativeQuery;
-import jakarta.persistence.NamedQuery;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
-@NamedQuery(query = "from User where id = :id", name = "User.byId")
-@NamedQuery(query = "from User where name = :name", name = "User.byName")
-@NamedNativeQuery(query = "select * from users where id = :id", name = "User.byIdNative", resultClass = User.class)
 public class User {
 
     @Id @GeneratedValue(strategy = GenerationType.SEQUENCE)
